5980ff0f9fc8615f0155648f7712bae62bdb6779
Beatriz Miranda <35241285+lastfirefly@users.noreply.github.com>
2023-09-20 17:08:00 -0300

    Create multip3.py

---
+++ multip3.py
@@ -0,0 +1,12 @@
+#Definição de Variável:
+
+x = int( input("Insira um número positivo: "))
+
+#Fazer a pesquisa dos valores múltiplos de 3:
+
+for x in range (0,x,1):
+    if ( x % 3 == 0):
+         print(x)
+         
+         
+
