0c26341e86871e1865f2fa891bc1c15fa413f89b
Beatriz Miranda <35241285+lastfirefly@users.noreply.github.com>
2023-09-20 17:01:25 -0300

    Create liq.py

---
+++ liq.py
@@ -0,0 +1,6 @@
+def liquidificar(item1, item2, item3):
+    if(item1 == "Banana" and item2 == "Leite" and item3 == "Aveia"):
+        print("Vitamina")
+    else:
+        print("Nova opção")
+liquidificar("Banana", "Uva", "Aveia")
